1672aa2ade604fbd5991ec6908cd782ecbcd787f
Sakib_Dalal <sakibdalal73@gmail.com>
2024-12-29 21:35:18 +0530

    Updated changes

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import requests
 from bs4 import BeautifulSoup
+import json
 
 from flask_cors import CORS
 
@@ -8,7 +9,7 @@ app = Flask(__name__)
 CORS(app) # to allow cross origin requests from react app
 
 LLM_URL = "http://localhost:11434/api/generate"
-header = {
+headers = {
     "Content-Type": "application/json"
 }
 
@@ -21,6 +22,20 @@ header = {
 # data = {
 #     "html_title": soup.text
 # }
+
+def llm_get(html_data):
+    data = {
+        "model": "llama3.2:1b",
+        "prompt": f"given the html data summarize it. data: {html_data}. Note: Dont say that you are using web scraping in output, you can add your own information also. Give response in English Language.",
+        "stream": False
+    }
+    try: 
+        llm_res = requests.post(LLM_URL, headers=headers, data=json.dumps(data))
+        llm_res.raise_for_status()
+        return llm_res.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error connecting to LLM: {e}")
+        return {"error": "Failed to connect to LLM service."}
 
 @app.route('/', methods=['GET'])
 def home():
@@ -35,9 +50,10 @@ def submit():
     response = requests.get(url=url)
     soup = BeautifulSoup(response.text, "html.parser")
     data = soup.text
-    ollam
+    llm_output = llm_get(data)
+
     return jsonify(
-        {"url": url}
+        {"llm_res": llm_output["response"]}
     )
 
 
